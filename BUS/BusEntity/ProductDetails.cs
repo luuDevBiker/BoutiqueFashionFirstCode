1fac8eb4fcedb593251b55ed30f33e896171e8f1
Lê Văn Lưu <87215931+luuDevBiker@users.noreply.github.com>
2022-06-07 15:51:17 +0700

    Update ProductDetails.cs

--- BUS/BusEntity/ProductDetails.cs
+++ BUS/BusEntity/ProductDetails.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,30 +8,13 @@ namespace BUS.BusEntity
 {
     public class ProductDetails
     {
-        public List<ProductOptions> productOptions { get; set; }
-        public Products products { get; set; }
-        public ProductVariants productVariants { get; set; }
-        public List<Options> options { get; set; }
-        public List<OptionValues> optionValues { get; set; }
-        public List<VariantValues> variantValues { get; set; }
-        public ProductDetails()
-        {
-            productOptions = new List<ProductOptions>();
-            products = new Products();
-            productVariants = new ProductVariants();
-            options = new List<Options>();
-            variantValues = new List<VariantValues>();
-            optionValues = new List<OptionValues>();
-        }
-
-        public ProductDetails(List<ProductOptions> productOptions, Products products, ProductVariants productVariants, List<Options> options, List<OptionValues> optionValues, List<VariantValues> variantValues)
-        {
-            this.productOptions = productOptions;
-            this.products = products;
-            this.productVariants = productVariants;
-            this.options = options;
-            this.optionValues = optionValues;
-            this.variantValues = variantValues;
-        }
+     public Guid productId { get; set; }
+        public Guid VariantId { get; set; }
+        public string? productsName { get; set; }
+        public Guid skuId { get; set; }
+        public float importPrice { get; set; }
+        public float price { get; set; }
+        public int quantity { get; set; }
+        public List<Option>? option { get; set; }
     }
 }
