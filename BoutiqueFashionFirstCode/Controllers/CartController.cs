5ef701f2c6f21d1b2c18336e329cad7df147c0d7
PhiThiTrangg <93480821+PhiThiTrangg@users.noreply.github.com>
2022-06-23 03:27:58 +0700

    Commet Luồng Trang và Bình

--- BoutiqueFashionFirstCode/Controllers/CartController.cs
+++ BoutiqueFashionFirstCode/Controllers/CartController.cs
@@ -20,25 +20,25 @@ namespace BoutiqueFashionFirstCode.Controllers
         {
             _cartService = cartService ?? throw new ArgumentNullException(nameof(cartService));
         }
-        [HttpGet("GetProductInCart/{userId}")]
-        public List<CartDto> GetCart(Guid userId)
+        [HttpGet("GetProductInCart/{userId}")] // Show các sản phẩm trong giỏ hàng là Query => dùng HTTP Methor là HttpGet
+        public List<CartDto> GetCart(Guid userId) // Truyền User Id để lấy được giỏ hàng của user Đó.
         {
-            return _cartService.GetProductInCart(userId);
+            return _cartService.GetProductInCart(userId);// gọi đến phương thức getProductIncart ở tầng Bus.
         }
         [HttpPost("AddProductInCart")]
         public bool AddCart([FromBody] CreatCartViewModel cart)
         {
             return _cartService.AddCart(cart);
         }
-        [HttpPut("UpdateProductInCart")]
-        public bool UpdateProductInCart([FromBody] UpdateCartViewModel cart)
+        [HttpPut("UpdateProductInCart")] 
+        public bool UpdateProductInCart([FromBody] UpdateCartViewModel cart) // UpdateCartViewModel Nhận các giá trị đầu vào. 
         {
-            return _cartService.UpdateCart(cart);
+            return _cartService.UpdateCart(cart); // gọi đến UpdateCart Service ở BUS.
         }
         [HttpDelete("DeteleProductInCart/{key}")]
         public bool DeteleProductInCart([FromODataUri] Guid key)
         {
-            return _cartService.RevoteItemIncart(key);
+            return _cartService.RemoveItemIncart(key);
         }
 
     }
